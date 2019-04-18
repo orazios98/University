db854a685580d2cd1465f5ea5173f8b63c8561f7
Orazio <orazio.sciuto98@gmail.com>
2019-04-18 11:59:57 +0200

    Aggiunto Esempio Adapter

--- Ingegneria del Software/Tutorato/17_04/src/com/company/Main.java
+++ Ingegneria del Software/Tutorato/17_04/src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ciao mondo");
+        Auto a1 = Garage.getBerlina(10000);
+        Auto a2 = Garage.getMini(100);
+
+        int t=10;
+
+        System.out.println(a1.getTipo() + " = " + a1.getDistanza(t));
+        System.out.println(a2.getTipo() + " = " + a2.getDistanza(t));
 	// write your code here
     }
 }
